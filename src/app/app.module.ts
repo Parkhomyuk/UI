19bda2bded38014c3c3ef6ac33dd1cd5500f77bd
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2018-07-22 01:20:11 +0300

    end with first plot correcting name main-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MainContentComponent } from './main-content/main-content.component';
 import {DashboardService} from "./services/dashboard.service";
-import { TasksComponent } from './main-content/tasks/tasks.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ShortPipe} from "./pipes/shorten.pipes";
-import { MessagesComponent } from './main-content/messages/messages.component';
-import { ActivityComponent } from './main-content/activity/activity.component';
-import { PlotCircleComponent } from './main-content/plot-circle/plot-circle.component';
+import {MainContentComponent} from "./mainContent/main-content.component";
+import {MessagesComponent} from "./mainContent/messages/messages.component";
+import {ActivityComponent} from "./mainContent/activity/activity.component";
+import {PlotCircleComponent} from "./mainContent/plot-circle/plot-circle.component";
+import {TasksComponent} from "./mainContent/tasks/tasks.component";
 
 
 @NgModule({
